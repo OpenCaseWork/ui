e60132b0925c843b5756a025226a04b1234901e4
Keith Katsma <kmkatsma@gmail.com>
2017-04-29 14:20:49 -0500

    added material data table demo code

--- src/app/shared/constituent-search/constituent-search.component.ts
+++ src/app/shared/constituent-search/constituent-search.component.ts
@@ -1,22 +1,20 @@
 import { MdDialogRef } from '@angular/material';
 import { Component } from '@angular/core';
+import { DataTableDemo } from '../material/data-table-demo/data-table-demo';
 
 @Component({
     selector: 'app-constituent-search',
     templateUrl: './constituent-search.component.html',
 })
 export class ConstituentSearchComponent {
+  public title: string; // 'Constituent Search';
+  public message: string;
+  public lastname: string;
+  public firstname: string;
 
-    public title: string; // 'Constituent Search';
-    public message: string;
-    public lastname: string;
-    public firstname: string;
+  constructor(public dialogRef: MdDialogRef<ConstituentSearchComponent>) {
+  }
 
-    constructor(public dialogRef: MdDialogRef<ConstituentSearchComponent>) {
-
-    }
-
-    search() {
-
-    }
+  search() {
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { CustomMaterialModule } from './custom-material.module';
 
 import { ConstituentSearchComponent } from './constituent-search/constituent-search.component';
@@ -8,17 +9,27 @@ import { ConstituentSearchService } from './constituent-search/constituent-searc
 import { MessageBoxComponent } from './message-box/message-box.component';
 import { MessageBoxService } from './message-box/message-box.service';
 import { NotFoundComponent } from './not-found.component';
+import { DataTableDemo } from './material/data-table-demo/data-table-demo';
+import { DataTableHeader } from './material/data-table-demo/data-table-header';
+import { MdDataTableModule } from './material/data-table/index';
+import { PaginationControl } from './material/data-table-demo/pagination-control';
+import { PeopleDatabase } from './material/data-table-demo/people-database';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    CustomMaterialModule
+    CustomMaterialModule,
+    NgxDatatableModule,
+    MdDataTableModule,
   ],
   declarations: [
     ConstituentSearchComponent,
     MessageBoxComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    DataTableDemo,
+    DataTableHeader,
+    PaginationControl,
   ],
   exports: [
     ConstituentSearchComponent,
@@ -27,7 +38,8 @@ import { NotFoundComponent } from './not-found.component';
   ],
   providers: [
     ConstituentSearchService,
-    MessageBoxService
+    MessageBoxService,
+    PeopleDatabase,
   ],
   entryComponents: [
     ConstituentSearchComponent,
