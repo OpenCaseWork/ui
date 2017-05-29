3b140abd2eba5fd65157afd893a759cd48fe317f
Keith Katsma <kmkatsma@gmail.com>
2017-05-29 14:57:39 -0500

    auto complete translation

--- src/app/constituent/name-address/name-address.component.ts
+++ src/app/constituent/name-address/name-address.component.ts
@@ -1,9 +1,11 @@
 ﻿import { Component, OnInit, Input, OnChanges, AfterViewInit, ViewChild, ViewChildren } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, AsyncValidator, AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs/observable';
 import { Constituent } from '../../models/constituents/constituents.models';
-import { ConstituentDomains, City, Suffix,
-        Title, PostalCode, Township, State } from '../../models/constituents/domains/constituents-domains.models';
+import {
+  ConstituentDomains, City, Suffix,
+  Title, PostalCode, Township, State
+} from '../../models/constituents/domains/constituents-domains.models';
 import { LogService } from '../../core/logging/log.service';
 import { AutoCompleteService } from '../../shared/control-services/auto-complete.service';
 import { ValidatorService } from '../../shared/control-services/validator.service';
@@ -30,8 +32,6 @@ export class NameAddressComponent implements OnInit, OnChanges, AfterViewInit {
   filteredStates: Observable<State[]>;
 
   cityControl = new FormControl();
-  suffixControl = new FormControl();
-  titleControl = new FormControl();
   postalControl = new FormControl();
   townshipControl = new FormControl();
   statesControl = new FormControl();
@@ -39,20 +39,20 @@ export class NameAddressComponent implements OnInit, OnChanges, AfterViewInit {
   public emailFormControl: FormControl;
 
   constructor(private logService: LogService,
-              private autoCompleteService: AutoCompleteService,
-              private validatorService: ValidatorService,
-              private formBuilder: FormBuilder) {
+    private autoCompleteService: AutoCompleteService,
+    private validatorService: ValidatorService,
+    private formBuilder: FormBuilder) {
     this.emailFormControl = this.validatorService.emailFormControl;
     this.createForm();
     this.federalId = this.nameAddressForm.controls['federalId'];
   }
 
   ngAfterViewInit() {
-      if (this.vc) {
-        this.logService.log('set focus to last name');
-        this.vc.nativeElement.focus();
-      }
+    if (this.vc) {
+      this.logService.log('set focus to last name');
+      this.vc.nativeElement.focus();
     }
+  }
 
   ngOnInit() {
     this.contacts = new Array<String>();
@@ -62,13 +62,18 @@ export class NameAddressComponent implements OnInit, OnChanges, AfterViewInit {
   ngOnChanges() {
     if (this.domains && this.domains.cities) {
       if (!this.filteredCities) {
-        this.filteredCities = this.autoCompleteService.filteredItem$<City>(this.domains.cities, this.cityControl, 'cityName');
-        this.filteredTitles = this.autoCompleteService.filteredItem$<Title>(this.domains.titles, this.titleControl, 'titleText');
-        this.filteredSuffixes = this.autoCompleteService.filteredItem$<Suffix>(this.domains.suffixes, this.suffixControl, 'suffixText');
-        this.filteredPostalCodes = this.autoCompleteService.filteredItem$<PostalCode>(this.domains.postalCodes, this.postalControl, 'code');
-        this.filteredTownships =
-          this.autoCompleteService.filteredItem$<Township>(this.domains.townships, this.townshipControl, 'townshipName');
-          this.filteredStates = this.autoCompleteService.filteredItem$<State>(this.domains.states, this.statesControl, 'stateCd');
+        this.filteredCities = this.autoCompleteService.filteredItem$<City>(
+          this.domains.cities, this.cityControl, 'cityName');
+        this.filteredTitles = this.autoCompleteService.filteredItemAbstract$<Title>(
+          this.domains.titles, this.nameAddressForm.controls['title'], 'titleText');
+        this.filteredSuffixes = this.autoCompleteService.filteredItemAbstract$<Suffix>(
+          this.domains.suffixes, this.nameAddressForm.controls['suffix'], 'suffixText');
+        this.filteredPostalCodes = this.autoCompleteService.filteredItem$<PostalCode>(
+          this.domains.postalCodes, this.postalControl, 'code');
+        this.filteredTownships = this.autoCompleteService.filteredItem$<Township>(
+          this.domains.townships, this.townshipControl, 'townshipName');
+        this.filteredStates = this.autoCompleteService.filteredItem$<State>(
+          this.domains.states, this.statesControl, 'stateCd');
         this.logService.log('domains loaded');
       }
     } else {
@@ -77,21 +82,23 @@ export class NameAddressComponent implements OnInit, OnChanges, AfterViewInit {
 
     // populate form, but only if have populated constituent
     if (this.constituent && this.constituent.lastName) {
-      this.nameAddressForm.patchValue(this.constituent, {onlySelf: true});
+      this.nameAddressForm.patchValue(this.constituent, { onlySelf: true });
     }
     this.logService.log('ngchanges');
   }
 
   createForm() {
     this.nameAddressForm = this.formBuilder.group({
-      lastName: ['', Validators.required ],
-      firstName: ['', Validators.required ],
-      federalId: ['', Validators.pattern(this.validatorService.ssnRegex())], // new FormControl(),
-      middleName: '', // new FormControl(),
-      nickName: '', // new FormControl(),
-      maidenName: '',
-      address1: '',
-      address2: ''
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      firstName: ['', [Validators.required, Validators.maxLength(50)]],
+      federalId: ['', Validators.pattern(this.validatorService.ssnRegex())],
+      middleName: ['', Validators.maxLength(50)],
+      nickName: ['', Validators.maxLength(30)],
+      maidenName: ['', Validators.maxLength(30)],
+      address1: ['', Validators.maxLength(80)],
+      address2: ['', Validators.maxLength(80)],
+      title: '',
+      suffix: ''
     });
   }
 
@@ -103,6 +110,7 @@ export class NameAddressComponent implements OnInit, OnChanges, AfterViewInit {
   updateConstituentFromForm(constituentToUpdate: Constituent): Constituent {
     const formModel = this.nameAddressForm.value;
     const saveConstituent: Constituent = Object.assign({}, constituentToUpdate, formModel);
+    saveConstituent.suffixId = this.autoCompleteService.getIdValue(this.domains.suffixes, saveConstituent.suffix, 'suffixText', 'suffixId');
     return saveConstituent;
   }
 
--- src/app/shared/control-services/auto-complete.service.ts
+++ src/app/shared/control-services/auto-complete.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { FormControl } from '@angular/forms';
+import { FormControl, AbstractControl } from '@angular/forms';
 
 @Injectable()
 export class AutoCompleteService {
@@ -8,7 +8,28 @@ export class AutoCompleteService {
   constructor() {
   }
 
+  getIdValue(array: any[], value: string, valueProperty: string, idProperty: string): number {
+    for (let index = 0; index < array.length; index++) {
+      let element = array[index];
+      if (element[valueProperty] === value) {
+        return element[idProperty];
+      }
+    }
+    return 0;
+  }
+
   filteredItem$<T>(list: T[], control: FormControl, propertyName: string ): Observable<T[]> {
+    let filteredItems: Observable<T[]>;
+     if (list) {
+       filteredItems = control.valueChanges
+        .startWith(null)
+        .map(item => item && typeof item === 'object' ? item[propertyName] : item)
+        .map(value => value ? this.filterGeneric<T>(list, value, propertyName) : list.slice());
+    }
+    return filteredItems;
+  }
+
+  filteredItemAbstract$<T>(list: T[], control: AbstractControl, propertyName: string ): Observable<T[]> {
     let filteredItems: Observable<T[]>;
      if (list) {
        filteredItems = control.valueChanges
